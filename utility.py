9df0822ca1e024918a851876bd2cfd0e19cac34d
Ben Ulmer <ulmerb@stanford.edu>
2017-12-09 20:33:21 -0800

    First glance

--- utility.py
+++ utility.py
@@ -1,5 +1,8 @@
+# -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
 import numpy as np
+import pickle
+import os
 
 #Pass in data as an array of the points we want to plot
 def createLineGraph(data):
@@ -9,3 +12,19 @@ def createLineGraph(data):
 	y = np.arange(len(data))
 	ax.plot(x,y)
 	plt.show()
+
+sizes = [20,40,80]
+algorithms = ['lowend','highend','online']
+count = 0
+averageCostsForTwenty = [0]*3
+for filename in os.listdir('results/'):
+	print filename
+	f= open('results/'+filename, 'rb')
+	result = pickle.load(f)
+	count += 1
+	for i, algorithm in enumerate(algorithms):
+		averageCostsForTwenty[i] += result[20][algorithm]['cost']
+
+for i, cost in enumerate(algorithms):
+	averageCostsForTwenty[i] = float(averageCostsForTwenty[i])/ count
+print averageCostsForTwenty
